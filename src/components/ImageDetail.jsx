f7063c264a58599a5ef2da0ff9a700c127da327e
Pratham <122166099+pratham40@users.noreply.github.com>
2024-08-12 21:04:20 +0530

    useImageDetail hook

--- src/components/ImageDetail.jsx
+++ src/components/ImageDetail.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import Image from "./Image";
 
 function ImageDetail() {
     const { photoId } = useParams();
